a575b22458b4e88e22e80896af258a3cc319b274
Lucrix1337 <81621126+Lucrix1337@users.noreply.github.com>
2025-05-29 10:04:45 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(() => {
+        document.querySelector('.nickname').classList.add('show');
+    }, 1000);
+    
+    setTimeout(() => {
+        document.querySelector('.social-links').classList.add('show');
+    }, 2000);
+});
